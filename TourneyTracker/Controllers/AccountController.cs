57a3577578536a894162ad85f9a9d92cdeef1d4a
benjamin <benjaminfeenstra@hotmail.com>
2015-04-08 14:01:51 +0200

    update account shizzle

--- TourneyTracker/Controllers/AccountController.cs
+++ TourneyTracker/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace TourneyTracker.Controllers
 {
@@ -14,9 +15,49 @@ namespace TourneyTracker.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Register(/*RegisterModel model*/string dummy)
+        {
+            if (ModelState.IsValid)
+            {
+                //voeg gebruiker toe aan de database
+                return Login();
+            }
+
+            return View(/*RegisterModel*/);
+        }
+
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(/*LoginModel model, */string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                bool userValid = default(bool);
+                //zoek database naar user en wachtwoord
+                if (userValid)
+                {
+                    FormsAuthentication.SetAuthCookie("username", false /* = remember me*/);
+
+                    //check for valid return url
+                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+
+            //Error's in model
+            return View(/*model*/);
         }
     }
 }
