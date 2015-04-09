466fc820023bf007c4f8fee343f1d63e23417e64
benjamin <benjaminfeenstra@hotmail.com>
2015-04-09 15:10:18 +0200

    account update en test lijst mvc toegevoegd op home/index

--- TourneyTracker/Controllers/HomeController.cs
+++ TourneyTracker/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TourneyTracker.Models.Home;
 
 namespace TourneyTracker.Controllers
 {
@@ -11,7 +12,29 @@ namespace TourneyTracker.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var model = new IndexModel();
+            model.TestList = new TestListModel();
+            model.TestList.List.Add(new TestListModelItem
+                {
+                    id = 1,
+                    text = "one"
+                });
+            model.TestList.List.Add(new TestListModelItem
+            {
+                id = 2,
+                text = "two"
+            });
+            return View(model);
+        }
+
+        public ActionResult AddTestItem(TestListModel TestList, int id, string text)
+        {
+            TestList.List.Add(new TestListModelItem
+            {
+                id = id,
+                text = text
+            });
+            return PartialView("~/Views/Home/EditorTemplates/TestListModel.cshtml", TestList);
         }
     }
 }
---
+++ TourneyTracker/Models/Home/TestListModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TourneyTracker.Models.Home
+{
+    public class TestListModel
+    {
+        public List<TestListModelItem> List {get;set;}
+
+        public TestListModel()
+        {
+            List = new List<TestListModelItem>();
+        }
+    }
+}
