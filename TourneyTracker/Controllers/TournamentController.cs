dc6dd28fc66aac589513acb30c88dbf7cacd8c0a
Pay4yourlife <joshua_judo12@hotmail.com>
2015-04-10 11:02:18 +0200

    Add Participants to the List is working now! :dancer:

--- TourneyTracker/Controllers/TournamentController.cs
+++ TourneyTracker/Controllers/TournamentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TourneyTracker.Models.Tournament;
 
 namespace TourneyTracker.Controllers
 {
@@ -14,7 +15,25 @@ namespace TourneyTracker.Controllers
             return View();
         }
 
+        // GET: CreateTournament
         public ActionResult CreateTournament()
+        {
+            CreateTournamentViewModel tournamentViewModel = new CreateTournamentViewModel();
+            return View(tournamentViewModel);
+        }
+
+        public ActionResult AddParticipant(CreateTournamentViewModel tournamentViewModel, string participantName)
+        {
+            Participant participant = new Participant();
+            participant.Name = participantName;
+            participant.Number = tournamentViewModel.Participants.Count + 1;
+            tournamentViewModel.Participants.Add(participant);
+            return PartialView("~/Views/Tournament/Partial/_ParticipantTable.cshtml", tournamentViewModel);
+        }
+
+        // POST: CreateTournament
+        [HttpPost]
+        public ActionResult CreateTournament(CreateTournamentViewModel tournamentViewModel)
         {
             return View();
         }
---
+++ TourneyTracker/Models/Tournament/CreateTournamentViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TourneyTracker.Models.Tournament
+{
+    public class CreateTournamentViewModel
+    {
+        public string TournamentName { get;set;}
+
+        public int TournamentType { get; set; }
+        public List<SelectListItem> TournamentTypes { get; set; }
+
+        public string TournamentTag { get; set; }
+
+        public List<Participant> Participants { get; set; }
+
+        public CreateTournamentViewModel()
+        {
+            this.TournamentTypes = new List<SelectListItem>();
+            this.Participants = new List<Participant>();
+        }
+    }
+}
