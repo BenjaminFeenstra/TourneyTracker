f85262c155ea17da2c511546bdbabd6398fa3fb2
benjamin <benjaminfeenstra@hotmail.com>
2015-04-14 12:53:52 +0200

    en de sql

---
+++ TourneyTracker/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TourneyTracker
+{
+    public static class UnitOfWork
+    {
+        private static TourneyTrackerDBEntities context = new TourneyTrackerDBEntities();
+
+        private static GenericRepository<account> accountRepository;
+
+        public static GenericRepository<account> AccountRepository
+        {
+            get
+            {
+                if (accountRepository == null)
+                {
+                    accountRepository = new GenericRepository<account>(context);
+                }
+                return accountRepository;
+            }
+        }
+    }
+}
