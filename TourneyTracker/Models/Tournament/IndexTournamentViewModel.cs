3bd4a5280b409b6b41007ff55066eb4a659ee4b9
Pay4yourlife <joshua_judo12@hotmail.com>
2015-04-20 09:13:40 +0200

    TournamentViewModel Index

---
+++ TourneyTracker/Models/Tournament/IndexTournamentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TourneyTracker.Models.Tournament
+{
+    public class IndexTournamentViewModel
+    {
+        public List<TournamentViewModel> Tournaments { get; set; }
+
+        public IndexTournamentViewModel()
+        {
+            this.Tournaments = new List<TournamentViewModel>();
+        }
+
+    }
+}
---
+++ TourneyTracker/Models/Tournament/TournamentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TourneyTracker.Models.Tournament
+{
+    public class TournamentViewModel
+    {
+        public int TournamentId { get; set; }
+        public string TournamentName { get; set; }
+        public int NumberParticipants { get; set; }
+
+    }
+}
